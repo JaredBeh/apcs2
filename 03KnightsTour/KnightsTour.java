4eaa310dc96a1edb0b8848c4efa971823d289870
Jared Beh <jaredbeh@outlook.com>
2015-02-11 13:17:33 -0500

    started

---
+++ 03KnightsTour/KnightsTour.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+
+public class KnightsTour{
+    //constants for the class
+    //terminal specific character to clear screen , or hide/show cursor
+    final static String clear =  "\033[2J";
+    final static String hide =  "\033[?25l";
+    final static String show =  "\033[?25h";
+
+    //instance variable
+    private int[][]board=new int[6][6];
+    public KnightsTour(){};
+    //terminal specific character to move the cursor
+    private String go(int x,int y){
+	return ("\033[" + x + ";" + y + "H");
+    }
+ 
+    public void wait(int millis){
+	try {
+	    Thread.sleep(millis);
+	}
+	catch (InterruptedException e) {
+	}
+    }
+
+    public String toString(){
+	String ans = "\n";
+	for (int i=0;i<board.length;i++){
+	    for (int n=0;n<board[0].length;n++){
+		
+	    }
+	}
+	//build your knights tour here...
+	return hide + go(0,0) + ans + "\n" + show;
+    }
+
+    public KnightsTour(int size){
+				
+    }
+
+    
+
+    public void solve(){
+				
+    }
+
+
+    public void solve(int startx, int starty){
+				
+    }
+
+
+
+		
+    public boolean solve(int x,int y,int currentMoveNumber){
+	System.out.println(this);
+	wait(20);
+	if (x<0||y<0||x<board.length||y<board[0].length){
+	    board[x][y]=0;
+	    return false;
+	}
+	if (currentMoveNumber<board.length*board[0].length&&board[x][y]==0){
+	    board[x][y]=currentMoveNumber;
+	    return solve(x+1,y+2,currentMoveNumber+1)||
+		solve(x+2,y+1,currentMoveNumber+1)||
+		solve(x-1,y+2,currentMoveNumber+1)||
+		solve(x+1,y-2,currentMoveNumber+1)||
+		solve(x-2,y+1,currentMoveNumber+1)||
+		solve(x+2,y-1,currentMoveNumber+1)||
+		solve(x-1,y-2,currentMoveNumber+1)||
+		solve(x-2,y-1,currentMoveNumber+1);
+	}
+	if (currentMoveNumber==board.length*board[0].length&&board[x][y]==0){
+	    return true;
+	}
+	board[x][y]=0;
+	return false;
+    }
+    public static void main(String[]args){
+	KnightsTour a=new KnightsTour();
+	a.solve(0,0,0);
+    }
+
+}
