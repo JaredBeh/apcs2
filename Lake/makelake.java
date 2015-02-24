004e6b746d2869c9a89aaa4a5407ce34e1ce218c
JaredBeh <jaredbeh@outlook.com>
2015-02-24 00:55:25 -0500

    im getting an identifier expected error idk im too tired to think

--- Lake/makelake.java
+++ Lake/makelake.java
@@ -1,15 +1,46 @@
 public class makelake{
     static int row,col,elev,num;
     static int[][] board;
+    static int[][] orders;
     public makelake(){
 	row=0;
 	col=0;
 	elev=0;
 	num=0;
     }
-    public static int create(int r,int c,int e,int n){
-	board=new int[r][c];
-	
+    public static int create(int n){
+	if (n==0){
+	    int ans=0;
+	    for (int i=0;i<board.length;i++){
+		for (int x=0;x<board[0].length;x++){
+		    ans+=board[i][x];
+		}
+	    }
+	    return ans*72*72;
+	}
+	int startrow=orders[n][0]-1;
+	int startcol=orders[n][1]-1;
+	int times=orders[n][2];
+	int greatest=0;
+	for (int r=startrow;r<startrow+3;r++){
+	    for (int c=startcol;c<startcol+3;c++){
+		if (greatest<board[r][c]){
+		    greatest=board[r][c];
+		}
+	    }
+	}
+	while (times>0){
+	    for (int r=startrow;r<startrow+3;r++){
+		for (int c=startcol;c<startcol+3;c++){
+		    if (greatest==board[r][c]){
+			board[r][c]-=1;
+		    }
+		}
+	    }
+	    times--;
+	    greatest--;
+	}
+	return create(n-1);
 	
     }
     public static void main(String[]args){
@@ -20,6 +51,7 @@ public class makelake{
 	int A;
 	int r=0;
 	int c=0;
+	board=new int[row][col];
 	for (A=4;A<row*col;A++){
 	    if (r<row){
 		board[r][c]=Integer.parseInt(args[A]);
@@ -31,7 +63,7 @@ public class makelake{
 		r=1;
 	    }
 	}
-	int[][]orders=new int[num][3];
+	orders=new int[num][3];
 	int B=0;
 	while(B<num){
 	    orders[B][0]=Integer.parseInt(args[A+(B*3)]);
@@ -40,4 +72,5 @@ public class makelake{
 	    B++;
 	}
     }
+    System.out.println(makelake.create(num));
 }
