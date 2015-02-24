6b145d3c77588574801cfe46f7868d4be0c693af
JaredBeh <jaredbeh@outlook.com>
2015-02-24 00:12:52 -0500

    ah yes i remember now the parseint command

--- Lake/makelake.java
+++ Lake/makelake.java
@@ -10,35 +10,34 @@ public class makelake{
     public static int create(int r,int c,int e,int n){
 	board=new int[r][c];
 	
+	
     }
     public static void main(String[]args){
-	if (args){
-	    row=(int)args[0];
-	    col=(int)args[1];
-	    elev=(int)args[2];
-	    num=(int)args[3];
-	    int A;
-	    int r=0;
-	    int c=0;
-	    for (A=4;A<row*col;A++){
-		if (r<row){
-		    board[r][c]=args[A];
-		    r++;
-		}
-		else{
-		    c++;
-		    board[0][c]=args[A];
-		    r=1;
-		}
+	row=Integer.parseInt(args[0]);
+	col=Integer.parseInt(args[1]);
+	elev=Integer.parseInt(args[2]);
+	num=Integer.parseInt(args[3]);
+	int A;
+	int r=0;
+	int c=0;
+	for (A=4;A<row*col;A++){
+	    if (r<row){
+		board[r][c]=Integer.parseInt(args[A]);
+		r++;
 	    }
-	    int[][]orders=new int[num][3];
-	    int B=0;
-	    while(B<num){
-		orders[B][0]=(int)args[A+(B*3)];
-		orders[B][1]=(int)args[A+(B*3)+1];
-		orders[B][2]=(int)args[A+(B*3)+2];
-		B++;
+	    else{
+		c++;
+		board[0][c]=Integer.parseInt(args[A]);
+		r=1;
 	    }
+	}
+	int[][]orders=new int[num][3];
+	int B=0;
+	while(B<num){
+	    orders[B][0]=Integer.parseInt(args[A+(B*3)]);
+	    orders[B][1]=Integer.parseInt(args[A+(B*3)+1]);
+	    orders[B][2]=Integer.parseInt(args[A+(B*3)+2]);
+	    B++;
 	}
     }
 }
