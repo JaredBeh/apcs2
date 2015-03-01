734d69952bd3e8ae0e98549841a710338f44103e
JaredBeh <jaredbeh@outlook.com>
2015-03-01 17:44:51 -0500

    bronze done

--- 04usico/makelake.java
+++ 04usico/makelake.java
@@ -8,16 +8,16 @@ public class makelake{
 	    int ans=0;
 	    for (int i=0;i<board.length;i++){
 		for (int x=0;x<board[0].length;x++){
-		    if (board[i][x]>elev){
-			ans+=board[i][x]-elev;
+		    if (board[i][x]<elev){
+			ans+=elev-board[i][x];
 		    }
 		}
 	    }
 	    return ans*72*72;
 	}
-	int startrow=orders[n][0]-1;
-	int startcol=orders[n][1]-1;
-	int times=orders[n][2];
+	int startrow=orders[n-1][0]-1;
+	int startcol=orders[n-1][1]-1;
+	int times=orders[n-1][2];
 	int greatest=0;
 	for (int r=startrow;r<startrow+3;r++){
 	    for (int c=startcol;c<startcol+3;c++){
@@ -45,6 +45,9 @@ public class makelake{
 	for (int i=0;i<args.length;i++){
 	    a+=""+args[i]+" ";
 	}
+	if (a.length()<1){
+	    a="4 6 22 2 28 25 20 32 34 36 27 25 20 20 30 34 24 20 20 20 20 30 20 20 14 14 20 20 1 4 4 1 1 10";
+	}
 	Scanner sc=new Scanner(a);
 	row=sc.nextInt();
 	col=sc.nextInt();
@@ -57,12 +60,12 @@ public class makelake{
 	    }
 	}
 	orders=new int[num][3];
-	for (int i=0;i<row;i++){
-	    for (int j=0;j<col;j++){
+	for (int i=0;i<num;i++){
+	    for (int j=0;j<3;j++){
 		orders[i][j]=sc.nextInt();
 	    }
 	}
-	solve(num);
-	System.out.println(Arrays.deepToString(board));
+	System.out.println(create(num));
+	//System.out.println(Arrays.deepToString(board));
     }
 }
