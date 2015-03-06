c1cf1038f4a83770037c90306e6b00011ba57d71
JaredBeh <jaredbeh@outlook.com>
2015-03-05 22:40:25 -0500

    got a working select

--- 06quick/Quick.java
+++ 06quick/Quick.java
@@ -1,14 +1,15 @@
 import java.util.*;
 public class Quick{
-    public void partition(int[]ary,int si,int ei){
+    public int quickSelect(int[]ary,int si,int ei,int num){
 	int[]D=new int[ary.length];
 	System.arraycopy(ary,0,D,0,si);
 	try{
 	    System.arraycopy(ary,ei+1,D,ei+1,ary.length-ei-1);
 	}catch(IndexOutOfBoundsException ex){};
 	Random rand=new Random();
-	int i=rand.nextInt(ei-si)+si;
+	int i=rand.nextInt(ei+1-si)+si;
 	int e=ei;
+	int s=si;
 	for (int n=si;n<=e;n++){
 	    if (n!=i){
 		if (ary[n]<ary[i]){
@@ -25,10 +26,17 @@ public class Quick{
 	D[si]=ary[i];
 	System.out.println(Arrays.toString(D));
 	System.out.println(ary[i]);
+	if (si==num){
+	    return D[si];
+	}
+	if (si<num){
+	    return quickSelect(D,si+1,e,num);
+	}
+	return quickSelect(D,s,si-1,num);
     }
     public static void main(String[]args){
 	int[]a={3,1,2,5,4,7,6,8,9,0};
 	Quick q=new Quick();
-	q.partition(a,3,9);
+	q.quickSelect(a,0,a.length-1,7);
     }
 }
