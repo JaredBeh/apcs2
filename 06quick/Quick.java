084eb65fe133172e90413602cd9270c651f3c732
JaredBeh <jaredbeh@outlook.com>
2015-03-08 23:52:04 -0400

    rewrote the thing now its working

--- 06quick/Quick.java
+++ 06quick/Quick.java
@@ -1,42 +1,38 @@
 import java.util.*;
 public class Quick{
     public int quickSelect(int[]ary,int si,int ei,int num){
-	int[]D=new int[ary.length];
-	System.arraycopy(ary,0,D,0,si);
-	try{
-	    System.arraycopy(ary,ei+1,D,ei+1,ary.length-ei-1);
-	}catch(IndexOutOfBoundsException ex){};
 	Random rand=new Random();
 	int i=rand.nextInt(ei+1-si)+si;
 	int e=ei;
 	int s=si;
-	for (int n=si;n<=e;n++){
-	    if (n!=i){
-		if (ary[n]<ary[i]){
-		    D[si]=ary[n];
-		    si++;
-		}
-		else{
-		    D[ei]=ary[n];
-		    ei--;
-		}
+	int test=ary[i];
+	int temp=0;
+        while (s<e){
+	    while (ary[s]<test){
+		if (s==ei)break;
+		s++;
 	    }
-	    System.out.println(Arrays.toString(D));
+	    while(ary[e]>test){
+		if (e==si)break;
+		e--;
+	    }
+	    temp=ary[s];
+	    ary[s]=ary[e];
+	    ary[e]=temp;
+	    System.out.println(Arrays.toString(ary));
 	}
-	D[si]=ary[i];
-	System.out.println(Arrays.toString(D));
-	System.out.println(ary[i]);
-	if (si==num){
-	    return D[si];
+	System.out.println(test);
+	if (s<num){
+	    return quickSelect(ary,e,ei,num);
 	}
-	if (si<num){
-	    return quickSelect(D,si+1,e,num);
+	else if(s>num){
+	    return quickSelect(ary,si,s,num);
 	}
-	return quickSelect(D,s,si-1,num);
+	return ary[s];
     }
     public static void main(String[]args){
 	int[]a={3,1,2,5,4,7,6,8,9,0};
 	Quick q=new Quick();
-	q.quickSelect(a,0,a.length-1,7);
+	q.quickSelect(a,0,a.length-1,5);
     }
 }
