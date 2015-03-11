c8853cda78e0131b6d33a428ab5c7b700617c2f7
JaredBeh <jaredbeh@outlook.com>
2015-03-11 00:09:18 -0400

    idgi

--- 06quick/Quick.java
+++ 06quick/Quick.java
@@ -7,39 +7,39 @@ public class Quick{
     public static void quicksort(int[]ary,int low,int high){
 	int l=0;
 	int h=high;
-	int i=rand.nextInt(high+1-low)+low;
-	int test=ary[i];
-	int temp=0;
-	while(l<h){
-	    while(ary[l]<test){
-		if (l==high)break;
-		l++;
-	    }
-	    if(ary[l]==test){
-		
-	    }
-	    temp=ary[l];
-	    ary[l]=ary[h];
-	    ary[h]=temp;
-	}
+	if (high>=low||ary.length==0)return;
+	int a=partition(ary,low,high);
+	quicksort(ary,low,a-1);
+	quicksort(ary,a+1,high);
+	System.out.println(Arrays.toString(ary));
     }
     public static int quickSelect(int[]ary,int num){
 	return quickSelect(ary,0,ary.length-1,num);
     }
     public static int quickSelect(int[]ary,int si,int ei,int num){
+	if (ei<=si)return si;
+	int a=partition(ary,si,ei);
+	int left=a-si;
+	if (left<num){
+	    return quickSelect(ary,a+1,ei,num-left-1);
+	}
+	if(left>num){
+	    return quickSelect(ary,si,a-1,num);
+	}
+	return a;
+    }
+    public static int partition(int[]ary,int s,int e){
 	Random rand=new Random();
-	int i=rand.nextInt(ei+1-si)+si;
-	int e=ei;
-	int s=si;
+	int i=rand.nextInt(e+1-s)+s;
 	int test=ary[i];
 	int temp=0;
         while (s<e){
 	    while (ary[s]<test){
-		if (s==ei)break;
+		if (s==e)break;
 		s++;
 	    }
 	    while(ary[e]>test){
-		if (e==si)break;
+		if (e==s)break;
 		e--;
 	    }
 	    temp=ary[s];
@@ -48,16 +48,11 @@ public class Quick{
 	    System.out.println(Arrays.toString(ary));
 	}
 	System.out.println(test);
-	if (s<num){
-	    return quickSelect(ary,e,ei,num);
-	}
-	else if(s>num){
-	    return quickSelect(ary,si,s,num);
-	}
 	return ary[s];
     }
     public static void main(String[]args){
 	int[]a={3,1,2,5,4,7,6,8,9,0};
-	Quick.quickSelect(a,0,a.length-1,5);
+	quicksort(a);
+	System.out.println(Arrays.toString(a));
     }
 }
