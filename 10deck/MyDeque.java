256b2a8eed9ececfe00c925e332a38d99d67d9b4
JaredBeh <jaredbeh@outlook.com>
2015-03-25 22:51:35 -0400

    fixed resize

--- 10deck/MyDeque.java
+++ 10deck/MyDeque.java
@@ -1,54 +1,48 @@
 import java.util.*;
 public class MyDeque<T>{
-    public Object[] data;
+    public T[] data;
     public int f,l,size;
     public MyDeque(){
 	this(10);
     }
     public MyDeque(int i){
-	f=1;l=0;size=0;
-	data=new Object[i];
+	f=-1;l=-1;size=0;
+	data=(T[])(new Object[i]);;
     }
     public String toString(){
 	return Arrays.toString(data);
     }
+    public String name(){return "beh.jared";}
     public void resize(){
-	if (size>=data.length){
-	    Object[]d2=new Object[size*2];
-	    for (int i=f;i<size;i++){
-		d2[i-f]=data[i];
-	    }
-	    if(f>l){
-		for (int i=0;i<=l;i++){
-		    d2[size+i-1]=data[i];
-		}
-	    }
-	    f=0;
-	    l=size;
-	    data=d2;
+	if(size<data.length)return;
+	T[]d2=(T[])(new Object[size*2]);
+	if(f==l){
+	    d2[0]=data[f];
+	}else if(f>l){
+	    System.arraycopy(data,f,d2,0,size-f);
+	    System.arraycopy(data,0,d2,size-f,f);
+	}else{
+	    System.arraycopy(data,f,d2,0,size);
 	}
+	data=d2;
     }
     public void addFirst(T value){
 	resize();
-	if (f==0){
-	    f=data.length;
+	size++;
+	if(f==-1||l==-1){
+	    data[0]=value;
+	    f=0;l=0;
+	    return;
 	}
+	if(f==0)f=data.length;
 	f--;
 	data[f]=value;
-	size++;
-    }
-    public void addLast(T value){
-	resize();
-	size++;
-	if(l==data.length-1)l=-1;
-	l++;
-	data[l]=value;
     }
     public static void main(String[]arr){
 	MyDeque<Integer>q=new MyDeque<Integer>();
 	for(int i=0;i<11;i++){
-	    //q.addFirst(-1*i);
-	    q.addLast(i);
+	    q.addFirst(-1*i);
+	    //q.addLast(i);
 	}
 	System.out.println(q);
     }
