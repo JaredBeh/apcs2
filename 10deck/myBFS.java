0489a4453199548cbe9af6228ddaaf4bcdd6078f
JaredBeh <jaredbeh@outlook.com>
2015-03-30 23:26:14 -0400

    barely did anything

---
+++ 10deck/myBFS.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class myBFS{
+    public class cord{
+	int x,y;
+	public cord(int a,int b){
+	    x=a;
+	    y=b;
+	}
+    }
+    Queue<myLNode<cord>> order=new LinkedList<myLNode<cord>>();
+    public void solve(){
+	
+    }
+}
