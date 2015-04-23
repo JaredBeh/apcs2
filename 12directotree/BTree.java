1c36102b95c25f3f9413f7b8f3ff79eaaeaf9379
Jared Beh <jaredbeh@outlook.com>
2015-04-23 13:01:33 -0400

    commit

---
+++ 12directotree/BTree.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+public class BTree<E>{
+    public static final int PRE_ORDER=0;
+    public static final int IN_ORDER=1;
+    public static final int POST_ORDER=2;
+    private TreeNode<E> root;
+    public Random r=new Random();
+    public BTree(){
+	root=null;
+    }
+    public void add(E d){
+	add(root,new TreeNode<E>(d));
+    }
+    public void add(TreeNode<E> curr,TreeNode<E> bn){
+	if(curr.hasSpace()==0)curr.setLeft(bn);
+	if(curr.hasSpace()==1)curr.setRight(bn);
+	int a=r.nextInt(2);
+    }
+    private class TreeNode<E>{
+	private TreeNode<E> left,right;
+	public E value;
+	public TreeNode(E v){
+	    value=v;
+	}
+	public E getValue(){
+	    return value;
+	}
+	public void setValue(E v){
+	    value=v;
+	}
+	public void setLeft(E v){
+	    left=new TreeNode(v);
+	}
+	public void setRight(E v){
+	    right=new TreeNode(v);
+	}
+	public E getLeft(){
+	    return left.getValue();
+	}
+	public E getRight(){
+	    return right.getValue();
+	}
+	public int hasSpace(){
+	    if(getLeft()==null)return 0;
+	    if(getRight==null)return 1;
+	    return -1;
+	}
+    }
+    public static void main(String arr){
+	
+    }
+}
