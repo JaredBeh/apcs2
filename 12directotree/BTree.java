507400c636fd09dff839726fc036906924923195
Jared Beh <jaredbeh@outlook.com>
2015-04-23 13:15:25 -0400

    nosuchmethod error in main

--- 12directotree/BTree.java
+++ 12directotree/BTree.java
@@ -9,13 +9,23 @@ public class BTree<E>{
     public BTree(){
 	root=null;
     }
+    public BTree(E d){
+	root=new TreeNode<E>(d);
+    }
     public void add(E d){
 	add(root,new TreeNode<E>(d));
     }
     public void add(TreeNode<E> curr,TreeNode<E> bn){
 	if(curr.hasSpace()==0)curr.setLeft(bn);
-	if(curr.hasSpace()==1)curr.setRight(bn);
-	int a=r.nextInt(2);
+	else if(curr.hasSpace()==1)curr.setRight(bn);
+	else{
+	    int a=r.nextInt(2);
+	    if(a==0){
+		add(curr.left,bn);
+		return;
+	    }
+	    add(curr.right,bn);
+	}
     }
     private class TreeNode<E>{
 	private TreeNode<E> left,right;
@@ -30,10 +40,16 @@ public class BTree<E>{
 	    value=v;
 	}
 	public void setLeft(E v){
-	    left=new TreeNode(v);
+	    left=new TreeNode<E>(v);
+	}
+	public void setLeft(TreeNode<E> t){
+	    left=t;
+	}
+	public void setRight(TreeNode<E> t){
+	    right=t;
 	}
 	public void setRight(E v){
-	    right=new TreeNode(v);
+	    right=new TreeNode<E>(v);
 	}
 	public E getLeft(){
 	    return left.getValue();
@@ -43,11 +59,14 @@ public class BTree<E>{
 	}
 	public int hasSpace(){
 	    if(getLeft()==null)return 0;
-	    if(getRight==null)return 1;
+	    if(getRight()==null)return 1;
 	    return -1;
 	}
     }
     public static void main(String arr){
-	
+	BTree<Integer> t=new BTree<Integer>();
+	for(int i=0;i<8;i++){
+	    t.add(i);
+	}
     }
 }
