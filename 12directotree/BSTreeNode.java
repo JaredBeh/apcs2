ea4739912c21aa837f73c9afa0db50b14fb369a4
Jared Beh <jaredbeh@outlook.com>
2015-04-30 13:16:56 -0400

    blah

---
+++ 12directotree/BSTreeNode.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+import java.util.*;
+
+public class BSTreeNode<T extends Comparable> {
+
+    private T data;
+    private BSTreeNode<T> left;
+    private BSTreeNode<T> right;
+    private int tally;
+
+    public BSTreeNode( T d ) {
+ 
+	data = d;
+	left = right = null;
+	tally=1;
+    }
+    
+    //accessors
+    public T getData() {
+	return data;
+    }
+    public BSTreeNode<T> getLeft() {
+	return left;
+    }
+    public BSTreeNode<T> getRight() {
+	return right;
+    }
+    public int getTally(){
+	return tally;
+    }
+
+    //mutators
+    public void setData( T d ) {
+	data = d;
+    }
+    public void setLeft( BSTreeNode<T> l ) {
+	left = l;
+    }
+    public void setRight( BSTreeNode<T> r ) {
+	right = r;
+    }
+    public void incrementTally(){
+	tally++;
+    }
+    public boolean decrementTally(){
+	if(tally>1){
+	    tally--;
+	    return true;
+	}
+	return false;
+    }
+}
+
+
+
