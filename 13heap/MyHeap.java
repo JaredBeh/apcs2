6e67dd51949fdb4cf27ff5216b7cad8a1ec8d651
JaredBeh <jaredbeh@outlook.com>
2015-05-12 22:48:52 -0400

    done with heap

--- 13heap/MyHeap.java
+++ 13heap/MyHeap.java
@@ -1,5 +1,8 @@
 import java.util.*;
 public class MyHeap{
+    public String name(){
+	return "beh.jared";
+    }
     public boolean maxornah;
     public int[]data=new int[7];
     public MyHeap(boolean isMax){
@@ -12,7 +15,7 @@ public class MyHeap{
 	return Arrays.toString(data);
     }
     public void resize(){
-	if(data[0]==data.length){
+	if(data[0]>=data.length-1){
 	    int[]d=new int[data[0]*2];
 	    System.arraycopy(data,0,d,0,data[0]);
 	    data=d;
@@ -33,43 +36,48 @@ public class MyHeap{
 	    c/=2;
 	}
     }
+    public int recompare(int n){
+	if(n>data[0]/2 ||
+	   (maxornah && data[n]>data[2*n] && data[n]>data[2*n+1]) ||
+	   (!maxornah && data[n]<data[2*n] && data[n]<data[2*n+1])){
+	    return -1;
+	}
+	if((maxornah && data[2*n]>data[2*n+1])||(!maxornah && data[2*n]<data[2*n+1])){
+	    return 0;
+	}
+	return 1;
+    }
     public int remove(){
 	if(data[0]==0)throw new IndexOutOfBoundsException();
-	data[0]--;
+	resize();
 	int ans=data[1];
 	data[1]=data[data[0]];
-	swapDown();
+	data[data[0]]=0;
+	int c=1;
+	int r=recompare(1);
+	while(r>-1){
+	    int temp=data[c];
+	    data[c]=data[2*c+r];
+	    data[2*c+r]=temp;
+	    c=c*2+r;
+	    r=recompare(c);
+	}
+	data[0]--;
 	return ans;
     }
-    public void swapDown(){
-	int i=1;
-	boolean left=false;
-	if(maxornah){
-	    while(data[i]<data[2*i]||data[i]<data[2*i+1]){
-		left=data[2*i]>data[2*i+1];
-		int temp=data[i];
-		if(left){
-		    data[i]=data[2*i];
-		    data[2*i]=temp;
-		    i*=2;
-		}
-		else{
-		    data[i]=data[2*i+1];
-		    data[2*i+1]=temp;
-		    i=i*2+1;
-		}
-		
-	    }
-	}
+    public int peek(){
+	if(data[0]<1)throw new IndexOutOfBoundsException();
+	return data[1];
     }
     public static void main(String[] args){
-	MyHeap a=new MyHeap();
+	MyHeap a=new MyHeap(false);
 	a.add(3);
 	a.add(4);
 	a.add(2);
 	a.add(10);
 	a.add(1);
 	a.add(11);
+	a.remove();
 	System.out.println(a);
     }
 }
