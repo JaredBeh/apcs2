5f493ffce0eec31d9883eec110b72b777b217cac
JaredBeh <jaredbeh@outlook.com>
2015-05-20 22:51:35 -0400

    I SPENT HOURS MESSING THIS UP AND I DID IT WITH TOP DOWN ORGANIZATION IN LIKE 10 MINUTES

--- 15heapsort/HeapSort.java
+++ 15heapsort/HeapSort.java
@@ -6,11 +6,36 @@ public class HeapSort{
 	data=d;
 	size=s;
     }
-    public static void HSort(){
-	Heapify(data,size);
-	for(int i=size-1;i>0;i--){
-	    swap(0,i);
-	    swapDown(0,i-1);
+    public static void HSort(int[] d,int s){
+	data=d;
+	size=s;
+	Heapify();
+	int c=size-1;
+	while(c>0){
+	    swap(0,c);
+	    c--;
+	    swapDown(0,c);
+	}
+    }
+    public static void Heapify(){
+	int i=size/2-1;
+	while(i>=0){
+	    swapDown(i,size-1);
+	    i--;
+	}
+    }
+    public static void swapDown(int start,int end){
+	int child;
+	int root=start;
+	while(root*2+1<=end){
+	    child=root*2+1;
+	    if(child+1<=end && data[child]<data[child+1]){
+		child++;
+	    }
+	    if(data[root]<data[child]){
+		swap(root,child);
+	    }
+	    root=child;
 	}
     }
     public static void swap(int a,int b){
@@ -18,35 +43,9 @@ public class HeapSort{
 	data[a]=data[b];
 	data[b]=temp;
     }
-    public static void Heapify(int size){
-	for(int i=size/2-1;i>=0;i--){
-	    swapDown(i,size);
-	}
-    }
-    public static void swapDown(int i,int end){
-	if(2*i+1>=end){
-	    return;
-	}
-	if(2*i+2>=end){
-	    if(data[i]<data[2*i+1]){
-		swap(i,2*i+1);
-		swapDown(2*i+1,end);
-	    }
-	    return;
-	}
-	int a=2*i+1;
-	if(data[a]<data[a+1]){
-	    a++;
-	}
-	if(data[i]<data[a]){
-	    swap(i,a);
-	    swapDown(a,end);
-	}
-    }
     public static void main(String[] args){
-	int[] b = {1,8,6,4,9,11,0,5};
-	HeapSort a=new HeapSort(b,b.length);
-	HeapSort.Heapify();
+	int[]a={1,8,6,4,9,11,0,5};
+	HeapSort.HSort(a,a.length);
 	System.out.println(Arrays.toString(HeapSort.data));
     }
 }
